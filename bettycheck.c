fe0c00b2e8c2879ad05cf874bf42e5134738d7eb
= <=>
2023-11-16 02:41:05 -0800

    betty changes

--- bettycheck.c
+++ bettycheck.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-
+#ifndef SHELL_COMPILE
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 printf("Hello, Betty!\n");
 return (0);
 }
+#endif
